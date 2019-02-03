11a1e69b2036c87813b972bf6a30f46a9eb7ed18
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-02-03 14:48:57 -0500

    New User Survey - Changed to using pickers for the parking lot and time selection

--- up_mobile/up_mobile/up_mobile/ParkingSurvey.xaml.cs
+++ up_mobile/up_mobile/up_mobile/ParkingSurvey.xaml.cs
@@ -23,12 +23,5 @@ namespace up_mobile
         //     
         //  }
 
-        // SwitchToggled function - Logic that will generate fields for day & time data when a switch
-        // is toggled in ParkingSurvey.xaml SurveyGrid field
-        void SwitchToggled(object sender, ToggledEventArgs e)
-        {
-           // ParkingSurvey.SurveyGrid.P1label.IsVisible = true;
-        }
-
     }
 }
