af1580bc11f18e2a598f969ed658431117c32ce0
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-02-14 11:23:24 -0500

    Settings Plugin - Added in the Settings plugin that is being used in the dataexchange branch.

---
+++ up_mobile/up_mobile/up_mobile/Helpers/Settings.cs
@@ -0,0 +1,41 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace up_mobile.Helpers
+{
+    public static class Settings
+    {
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+        public static string Username
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("Username", "");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("Username", value);
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("Password", "");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("Password", value);
+            }
+        }
+    }
+}
