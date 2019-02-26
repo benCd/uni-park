2ecfef4a04bdfa3c4b20110b8f6eadb88515085a
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-02-25 20:10:26 -0500

    Parking Status pages added - Added ParkedButton and ParkedConfirm pages into the project - ParkedButton page has been added to the User set of tabbed pages and appears as "Parking Status" to the user - ParkingButton navigates to ParkingConfirm page and is displayed as "Confirmation" for the act of pressing the "I Parked" button on ParkedButton page

---
+++ up_mobile/up_mobile/up_mobile/ParkedButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace up_mobile
+{
+    /// <summary>
+    /// ParkedButton page - Where the user goes to tell the app they have parked
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParkedButton : ContentPage
+    {
+        /// <summary>
+        /// Loads ParkedButton page <see cref="ParkedButton.xaml"/>
+        /// </summary>
+        public ParkedButton()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When the I Parked button on <see cref="ParkedButton.xaml"/> is pressed
+        /// navigates to the ParkedConfirm page <see cref="ParkedConfirm.xaml"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void ParkedButtonPressed(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await Navigation.PushAsync(new ParkedConfirm());
+        }
+    }
+}
---
+++ up_mobile/up_mobile/up_mobile/ParkedConfirm.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace up_mobile
+{
+    /// <summary>
+    /// ParkedConfirm page - Where the user confirms that they parked after 
+    /// pressing I Parked button on <see cref="ParkedButton.xaml"/> page
+    /// and the app uses GPS to detect which lot they are in
+    /// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ParkedConfirm : ContentPage
+	{
+        /// <summary>
+        /// Loads the ParkedConfirm page <see cref="ParkedConfirm.xaml"/>
+        /// </summary>
+		public ParkedConfirm ()
+		{
+			InitializeComponent ();
+		}
+
+	}
+}
