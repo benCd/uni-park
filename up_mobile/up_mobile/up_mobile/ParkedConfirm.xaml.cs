ab7d3b8ebedc9bfd8dcbcc20771a879c40f39782
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-02-25 21:10:06 -0500

    Slider Implemented - Implemented a basic slider on ParkingConfirm page - Updated it's Background and Button color - Updated it to slide between 0 and 100 - Typecast the value to an Integer and displayed it to the user as a percentage

--- up_mobile/up_mobile/up_mobile/ParkedConfirm.xaml.cs
+++ up_mobile/up_mobile/up_mobile/ParkedConfirm.xaml.cs
@@ -25,5 +25,43 @@ namespace up_mobile
 			InitializeComponent ();
 		}
 
-	}
+        /// <summary>
+        /// When Confirm button on <see cref="ParkedConfirm.xaml"/> is pressed
+        /// it sends the information to the server and locks the I Parked button
+        /// on <see cref="ParkedButton"/> page for 10 minutes, then navigates back to Home <see cref="Home.xaml"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void ConfirmPressed(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await Navigation.PushAsync(new User());
+        }
+
+        /// <summary>
+        /// When the Deny button on <see cref="ParkedConfirm.xaml"/> is pressed
+        /// it navigates back to <see cref="ParkedButton.xaml"/> and unlocks the I Parked button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void DenyPressed(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await Navigation.PushAsync(new ParkedButton());
+        }
+
+        /// <summary>
+        /// When the Slider is moved on <see cref="ParkedConfirm"/> it updates what
+        /// is shown on the page just above the slider, letting users know what value
+        /// they are reporting for Lot fullness
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void SliderUpdated(object sender, ValueChangedEventArgs args)
+        {
+            int value = (int) args.NewValue;
+            displayLabel.Text = String.Format("Lot Fullness is {0} %", value);
+        }
+
+    }
 }
