3ba1cdcce04a0d380f4428a8cc3d83c7e26c20e3
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-03-02 17:31:55 -0500

    Locking Parked Button - Currently the button has been made to lock, but doesn't unlock properly

--- up_mobile/up_mobile/up_mobile/ParkedButton.xaml.cs
+++ up_mobile/up_mobile/up_mobile/ParkedButton.xaml.cs
@@ -33,6 +33,19 @@ namespace up_mobile
         {
             Button button = (Button)sender;
             await Navigation.PushAsync(new ParkedConfirm());
+
+            // Sets the button to be disabled
+            button.IsEnabled = false;
+
+            // Stop watch for use in unlocking the button, unlocks after 10,000 ms (10 seconds)
+            System.Diagnostics.Stopwatch stopper = System.Diagnostics.Stopwatch.StartNew();
+
+            if (stopper.ElapsedMilliseconds >= 10000) //600,000 for 10 minutes
+            {
+                
+                button.IsEnabled = true;
+                stopper.Stop();
+            }
         }
     }
 }
