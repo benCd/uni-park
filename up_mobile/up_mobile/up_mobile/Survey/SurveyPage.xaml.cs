d58a32d355f31f33a1424a30ceb2dd6c06784405
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-03-10 14:09:58 -0400

    SurveyPage added to the project. It is to replace MondaySurvey through SundaySurvey pages once it is finished.

---
+++ up_mobile/up_mobile/up_mobile/Survey/SurveyPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace up_mobile
+{
+    /// <summary>
+    /// SurveyPage page
+    /// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SurveyPage : ContentPage
+	{
+        /// <summary>
+        /// Loads SurveyPage  page
+        /// </summary>
+		public SurveyPage ()
+		{
+			InitializeComponent ();
+		}
+
+        /// <summary>
+        /// When the Next Page button on SurveyPage page 
+        /// <see cref="SurveyPage.xaml"/> is pressed it navigates 
+        /// to the next version of the page
+        /// </summary>
+        async void NextSurveyPageButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            //await Navigation.PushAsync(new SurveyPage());
+        }
+    }
+}
