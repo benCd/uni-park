07823d2f66858ca2c3f0ecb424683082c4d2ac3b
benCd <benji21997@gmail.com>
2019-03-27 11:34:26 -0400

    Added colour mapper

---
+++ up_mobile/up_mobile/up_mobile/Map/Utils/ColorMapper.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace up_mobile.Map.Utils
+{
+    public class ColorMapper
+    {
+        public static Color MapPercentageToRGB(double percentage)
+        {
+            /*
+             * Start: 132, 255, 132
+             * Medium: 255, 255, 132
+             * End: 255, 132, 132
+             */
+
+            return Color.FromRgb(
+                255 - ((percentage <= 0.5) ? 123 * percentage*2 : 0), 
+                255 - ((percentage > 0.5) ? 123 * (percentage-0.5)*2 : 0), 
+                132);
+        }
+    }
+}
