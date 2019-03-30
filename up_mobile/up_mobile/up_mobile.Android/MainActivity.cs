ea30887a04b4fb87f3270b6468dc34816bd36386
ClayBullard <cbullard@oakland.edu>
2019-03-30 11:56:47 -0400

    firebase attempt

--- up_mobile/up_mobile/up_mobile.Android/MainActivity.cs
+++ up_mobile/up_mobile/up_mobile.Android/MainActivity.cs
@@ -4,16 +4,86 @@ using Android.OS;
 using Plugin.CurrentActivity;
 using Xamarin.Forms;
 using Android.Content;
+//firebase
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
 
 namespace up_mobile.Droid
 {
     [Activity(Label = "up_mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        
+        //firebase business
+        static readonly string TAG = "FCM";
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                Log.Debug(TAG, "Google Play Services is available.");
+                return true;
+            }
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            //firebase
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+            IsPlayServicesAvailable();
+
+            CreateNotificationChannel();
+
+            //subscribe to topic
+            FirebaseMessaging.Instance.SubscribeToTopic("news");
+
+            Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+            //end of firebase
 
             //Init for Rg.Plugins.Popup
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
@@ -37,8 +107,6 @@ namespace up_mobile.Droid
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
-            
 
             base.OnCreate(savedInstanceState);
             // the following call to the cross current activity plugin is required by the GPS plugin
