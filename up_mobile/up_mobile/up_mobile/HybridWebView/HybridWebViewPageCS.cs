5c1eeafd6d1a563d4fb3f1f22ba31c5cc87c9c7d
Adam Hamed <44811380+Adam40471@users.noreply.github.com>
2019-04-01 21:41:19 -0400

    ll

---
+++ up_mobile/up_mobile/up_mobile/HybridWebView/HybridWebViewPageCS.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace up_mobile
+{
+    public class HybridWebViewPageCS : ContentPage
+    {
+        public HybridWebViewPageCS()
+        {
+            var CalendarAuthWebView = new HybridWebView()
+            {
+                Uri = "https://www.google.com",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            CalendarAuthWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+
+            Padding = new Thickness(0, 20, 0, 0);
+            Content = CalendarAuthWebView;
+        }
+    }
+}
